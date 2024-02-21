8e07669b5385e6400be3f05f88b7f6c1197333c1
abhiarya2409 <39700333+abhiarya2409@users.noreply.github.com>
2024-02-21 12:24:24 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div className="container my-3">
+        <h3>About Us</h3>
+        <p>
+          Welcome to My Todos List, a simple and intuitive todo application built with React. 
+          Manage your tasks efficiently and stay organized with our user-friendly interface.
+        </p>
+        <p>
+          This application is created by Abhishek Arya as a demonstration of React.js and its capabilities.
+        </p>
+      </div>
+    )
+}
